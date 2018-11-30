e2ea41531d5c3e488f43a3af358570ddfe329d78
Jesús Mesa González <sulimesa@gmail.com>
2018-11-30 13:23:25 +0100

    Corregir puerto escucha con flask #9

--- app.py
+++ app.py
@@ -208,5 +208,4 @@ def setDireccion():
 if __name__ == "__main__":
     
     # Lanzar aplicacion.
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port="80")
