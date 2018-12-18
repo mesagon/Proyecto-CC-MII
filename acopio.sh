dfc0f9389f9d567d2dab8d06249cadef64d75175
Jesús Mesa González <sulimesa@gmail.com>
2018-12-18 14:43:51 +0100

    Corregir script. closes #18

--- acopio.sh
+++ acopio.sh
@@ -6,7 +6,7 @@ az group create --location francecentral --name Hito4-Francia-Central
 
 # Crear la máquina virtual.
 echo "Creando maquina virtual."
-az vm create --resource-group Hito4-Francia-Central --name GestionClientes --image credativ:Debian:9:9.0.201808270 --admin-username azureuser --ssh-key-value ~/.ssh/id_rsa.pub --size Basic_A1 --public-ip-address-allocation dynamic
+az vm create --resource-group Hito4-Francia-Central --name GestionClientes --image credativ:Debian:9:9.0.201808270 --admin-username azureuser --ssh-key-value ~/.ssh/id_rsa.pub --size Basic_A1 --public-ip-address-allocation static
 
 # Abrir el puerto 80.
 echo "Abriendo el puerto 80."
