913e05086bc4b5a68d6f22eeb72465fc7f1138dc
Jesús Mesa González <sulimesa@gmail.com>
2018-12-18 14:42:16 +0100

    Añadir script de acopio #18

---
+++ acopio.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Crear grupo de recursos.
+echo "Creando grupo de recursos."
+az group create --location francecentral --name Hito4-Francia-Central
+
+# Crear la máquina virtual.
+echo "Creando maquina virtual."
+az vm create --resource-group Hito4-Francia-Central --name GestionClientes --image credativ:Debian:9:9.0.201808270 --admin-username azureuser --ssh-key-value ~/.ssh/id_rsa.pub --size Basic_A1 --public-ip-address-allocation dynamic
+
+# Abrir el puerto 80.
+echo "Abriendo el puerto 80."
+az vm open-port --port 80 --resource-group Hito4-Francia-Central --name GestionClientes
